37798aee2f591ae6647c36a721523af7e387e163
krola1 <kwl082024@gmail.com>
2026-04-14 12:27:52 +0200

    bang

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import Example from "./components/Example";
 import Parent from "./components/Parent";
 
 function App() {
+  const message = "hei fra app";
   return (
     <>
-      <h3>Message location</h3>
+      <Example>
+        <h3>Message location</h3>
+      </Example>
       <Parent />
     </>
   );
---
+++ src/components/Example.jsx
@@ -0,0 +1,3 @@
+export default function Example({ children }) {
+  return <div style={{ color: "red" }}>{children}</div>;
+}
